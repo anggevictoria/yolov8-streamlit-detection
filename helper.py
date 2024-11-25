69ef0ecfa90c128371151eea9a5cebb431ec65f5
angge <166150697+anggevictoria@users.noreply.github.com>
2024-11-25 23:04:07 +0800

    cuda

--- helper.py
+++ helper.py
@@ -9,31 +9,49 @@ def load_model(model_path):
     return model
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
-    image = cv2.resize(image, (720, int(720 * (9 / 16))))
+def _display_detected_frames(conf, model, st_frame, image):
+    # Resize the image to a fixed width of 160 pixels while maintaining a 16:9 aspect ratio
+    aspect_ratio = 9 / 16
+    new_width = 160
+    new_height = int(new_width * aspect_ratio)
+    image = cv2.resize(image, (new_width, new_height))
+    model.to('cuda')
+    
+    # Perform object detection
     res = model.predict(image, conf=conf)
 
+    # Plot the results
     res_plotted = res[0].plot()
+
+    # Display the image with detected objects
     st_frame.image(res_plotted, caption='Detected Video', channels="BGR", use_container_width=True)
+    
+    
+
 
 def play_webcam(conf, model):
     source_webcam = settings.WEBCAM_PATH
     if st.button('Detect Objects'):
             vid_cap = cv2.VideoCapture(source_webcam)
             st_frame = st.empty()
+            detected_objects = set()  # Initialize a set to store unique object names
+
             while vid_cap.isOpened():
+                
+
                 success, image = vid_cap.read()
                 if success:
-                    _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None)
+                    _display_detected_frames(conf, model, st_frame, image)
                 else:
                     vid_cap.release()
                     break
 
-# extract the name of detectedd object
+# extract the name of detected object
 def get_detected_object_names(boxes, class_names):
     detected_objects = set()
     for box in boxes:
         class_id = int(box.cls)
+        
         # Map class ID to object name
         object_name = class_names[class_id]
         detected_objects.add(object_name)
