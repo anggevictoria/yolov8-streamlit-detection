109654830b5181d3d0663a4ba628875ed5e57bd1
angge <166150697+anggevictoria@users.noreply.github.com>
2024-11-25 11:28:27 +0800

    object description 3 sec

--- app.py
+++ app.py
@@ -94,7 +94,8 @@ if source_radio == settings.IMAGE:
             st.image(res_plotted, caption='Detected Image',
                     use_container_width=True)
                     
-            detected_objects = set()  # Initialize an empty set to track detected object names
+            detected_objects_list = [] # Initialize a list to keep the order of detections
+
 
             try:
                 for box in boxes:
@@ -102,14 +103,30 @@ if source_radio == settings.IMAGE:
                                 x1, y1, x2, y2, confidence, class_id = box.data[0][:6]  # Adjust indices if `box.data` has a different structure
                                 object_name = model.names[int(class_id)]  # Map class ID to name
 
-                                if object_name in detected_objects:
+                                if object_name not in detected_objects_list:
+                                    #add the object to the set and process
+                                    detected_objects_list.append(object_name) # Add the object to the list at the last position
+
+                                else:
                                     continue
 
-                                #add the object to the set and process
-                                detected_objects.add(object_name)            
-                                description = helper.generate_description(object_name)
-                                st.write(f"{object_name} detected: {description}. Ask our streaming chatbot if you want to know more about {object_name}")
-                        
+                                # Use the first item in the list (the first detected object)
+                                first_detected_object = detected_objects_list[0]
+                                description = helper.generate_description(first_detected_object)
+                               
+                                # Empty container to hold the message
+                                msg_container = st.empty()    
+        
+                                msg_container.write(f"{first_detected_object} detected: {description}. Ask Streaming chatbot if you want to know more about {detected_objects_list[0]}")
+                                time.sleep(3)  # Wait for 3 seconds before clearing the message
+                                msg_container.empty()  # Remove the message after 3 seconds
+
+                                # Remove the first item from the set and list after processing
+                                detected_objects_list.pop(0)  # Remove the first item in the list
+
+
+
+                                                        
                                 
             except Exception as ex:
                 st.write("No image is uploaded yet!")        
