bab148d2b81bb86708faf3da6213f7c18f95fa92
angge <166150697+anggevictoria@users.noreply.github.com>
2024-11-30 19:01:02 +0800

    adding comlab

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'CENAR_detected.png'
 # Videos config
 VIDEO_DIR = ROOT / 'videos'
 VIDEOS_DICT = {
-    'video_1': VIDEO_DIR / 'video_1.mp4',
+    'video_1': VIDEO_DIR / 'ComLab1.mp4',
     'video_2': VIDEO_DIR / 'video_2.mp4',
     'video_3': VIDEO_DIR / 'video_3.mp4',
 }
